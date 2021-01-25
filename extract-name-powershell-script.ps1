f089fc7c0da99e6891dee6d1e376ec31c1f7c12e
Mohammed Saklain Shahab <68351176+saklain-create@users.noreply.github.com>
2021-01-25 22:41:28 +0530

    Add files via upload
    main code for extracting name from path stored in variable

---
+++ extract-name-powershell-script.ps1
@@ -0,0 +1,48 @@
+﻿<#
+.SYNOPSIS
+	extract-name-powershell-script
+
+.DESCRIPTION
+    $var="F:\ANNOOR\NEW BOT\sample.docx" and dinanamicly extract 
+    Sample.docx using a script file to present in a variable
+    in a new variable $3= Sample.docx 
+    
+.NOTES
+	File Namespace	: extract-name-powershell-script.ps1
+	Version			: 0.01
+	Last Modified	: 25-01-2021
+
+.LINK
+	Source Code
+	https://github.com/saklain-create/extract-name-powershell-script
+#>
+
+
+#Variable to extrated
+$var= read-host -prompt "PATH to extrated"
+
+#temporary path
+$path = READ-HOST -PROMPT "temporary file PATH"
+
+#Extart 
+new-item -path "$Path\temp" -itemtype directory
+new-item "$Path\temp\temp.csv"
+$1 = LS -path "$var" | Format-Wide
+echo $1 >> "$Path\temp\temp.csv"
+$2 = import-csv -path "$Path\temp\temp.csv" | get-random
+$3 = $2.H1
+$4 = $3.Contains("Directory")
+
+while ( $4 -eq "true"){
+Remove-Variable 2
+Remove-Variable 3
+Remove-Variable 4
+
+$2 = import-csv -path "$Path\temp\temp.csv" | get-random
+$3 = $2.H1
+$4 = $3.Contains("Directory") }
+
+#clean the temp folder
+Remove-Item -Path "$Path\temp" -Recurse
+
+$3 #extrated name
